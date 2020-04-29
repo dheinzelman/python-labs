78f12e984d06aad932483d316d8041c17aa7a09d
Daniel <8408772+dheinzelman@users.noreply.github.com>
2020-04-28 20:34:44 -0400

    Completed 02_01_cylinder.py

--- labs/02_basic_datatypes/1_numbers/02_01_cylinder.py
+++ labs/02_basic_datatypes/1_numbers/02_01_cylinder.py
@@ -4,3 +4,10 @@ of a cylinder with a radius of 3.14 and a height of 5. Print out the result.
 
 
 '''
+pi=22/7
+height = float(input('Height of cylinder: '))
+radian = float(input('Radius of cylinder: '))
+volume = pi * radian * radian * height
+sur_area = ((2*pi*radian) * height) + ((pi*radian**2)*2)
+print("Volume is: ", volume)
+print("Surface Area is: ", sur_area)
