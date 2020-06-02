7f9ad6dccdd03d4878b455678836c454c7d9f578
Daniel <8408772+dheinzelman@users.noreply.github.com>
2020-06-02 17:39:20 -0400

    completed

--- labs/03_more_datatypes/2_lists/03_06_product_largest.py
+++ labs/03_more_datatypes/2_lists/03_06_product_largest.py
@@ -9,3 +9,14 @@ that we haven't looked at yet. See if you can figure it out, otherwise
 come back to this task after you have learned about loops)
 
 '''
+list1 = []
+
+for a in range(0,10):
+    number = int(input("please enter a number: "))
+    list1.append(number)
+
+mul=1
+for i in range(0,10):
+    mul=mul*list1[i]
+
+print(mul)
